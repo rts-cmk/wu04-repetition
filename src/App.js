094e4911b784a32e202a16b29bd706eea1627a70
Brian Emilius <brian@brianemilius.com>
2021-09-14 12:36:08 +0200

    usestate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useState } from "react";
+
+function MyComponent() {
+  // Den destrukturerede metode
+  // første element i arrayet er statet,
+  // andet element i arrayet er en funktion, som ændrer værdien
+  // af det første element i arrayet.
+  var [on, setOn] = useState(false)
+
+  return (
+  <>
+  <div style={{
+    width:100,
+    height:100,
+    backgroundColor: on ? "yellow" : "black"
+  }}></div>
+  <button onClick={() => setOn(!on)}>CLEECK MEH!! xD</button>
+  </>
+)
+}
+
+
 function App() {
   return (
-    null
+    <MyComponent text="Hej" />
   );
 }
 
