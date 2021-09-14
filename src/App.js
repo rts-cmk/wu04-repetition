669d0af05ebb6fbb7747d8acc864fbe910e8adc9
Brian Emilius <brian@brianemilius.com>
2021-09-14 14:50:08 +0200

    thingies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios"
+
 function App() {
+  var [content, setContent] = useState([])
+
+  useEffect(function () {
+    axios.post("http://localhost:4000/auth/token",
+      { username: "user1", password: "1234" })
+      .then(function (response) {
+        if (response.status !== 200)
+          throw new Error("BLARGH!")
+
+        setContent(response.data)
+        console.log(response.data)
+      })
+      .catch(error => console.error(error))
+  }, [])
+
   return (
-    null
+    <p>{content.userId}</p>
   )
 }
 
