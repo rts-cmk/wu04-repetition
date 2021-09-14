08eb3b69f63257ecbf2f59e0f85420bde86331f3
Brian Emilius <brian@brianemilius.com>
2021-09-14 13:01:41 +0200

    useeffect1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  // dette state bliver vist ved componentWillMount
+  var [content, setContent] = useState([
+    { className: "..." }
+  ])
+
+  // Funktionen i denne useEffect-hook bliver kørt
+  // én gang, ved componentDidMount (fordi der er et
+  // tomt dependency-array)
+  useEffect(function() {
+    fetch("http://localhost:4000/api/v1/classes")
+      .then(res => res.json())
+      .then(data => setContent(data))
+  }, [])
+
   return (
-    null
+    <>
+      <h1>Hold</h1>
+      {content.map(element => <p>{element.className}</p>)}
+    </>
   );
 }
 
