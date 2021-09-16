feab6f3a1f6d7645b9e916cf17bddca54e293fc3
Brian Emilius <brian@brianemilius.com>
2021-09-16 11:30:28 +0200

    cookies for everyone

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,22 @@
 import { navigate } from "@reach/router"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import Store from "./Store"
+import { useCookies } from "react-cookie"
 
 export default function Login() {
 	var {setToken} = useContext(Store)
+	var [cookies] = useCookies(["bf-token"])
+	
+	useEffect(function() {
+		if (cookies["bf-token"]) {
+			setToken(cookies["bf-token"])
+			navigate("/myschedule")
+		}
+	}, [])
 
 	function submitHandler(event) {
 		event.preventDefault()
+		
 		fetch("http://localhost:4000/auth/token", {
 			method: "POST",
 			headers: {
@@ -20,6 +30,12 @@ export default function Login() {
 		.then(res => res.json())
 		.then(data => {
 			setToken(data)
+
+			if (event.target.rememberme.checked) {
+				var expires = new Date(data.validUntil).toUTCString()
+				document.cookie = `bf-token=${JSON.stringify(data)};expires=${expires}`
+			}
+
 			navigate("/myschedule")
 		})
 	}
@@ -37,6 +53,10 @@ export default function Login() {
 					Adgangskode
 				</label>
 				<input type="password" name="password" />
+			</div>
+			<div>
+				<input type="checkbox" name="rememberme" />
+				<label>Husk mig</label>
 			</div>
 			<button type="submit">Log ind</button>
 		</form>
